86b07a5c1c77e2d8130cf132a342a9e9c7e54ddc
Vladyslav <sharpilo.vladislav96@gmail.com>
2023-08-29 21:11:48 +0300

    Фронтенд и работа с файлами

--- app/main.py
+++ app/main.py
@@ -1,34 +1,38 @@
-from fastapi import FastAPI, Query
-from typing import Optional
-from datetime import date
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
 
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
+
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory='app/static'), 'static')
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
 
+app.include_router(router_pages)
+app.include_router(router_images)
 
-class HotelsSearchArgs:
-    def __init__(
-        self,
-        location: str,
-        date_from: date,
-        date_to: date,
-        has_spa: Optional[bool] = Query(None, description="Whether the hotel has a spa or not (bool).", enum=[True, False]),
-        stars: Optional[int] = Query(None, ge=1, le=5, description="Number of stars for the hotel (int)."),
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.has_spa = has_spa
-        self.stars = stars
+origins = [
+    "http://localhost:8000",
+]
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", 
+                   "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", 
+                   "Authorization"],
+)
