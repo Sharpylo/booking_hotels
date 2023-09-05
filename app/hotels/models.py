c8fac85c8e1d2af6fa78ab9ac9bc9f28f3b8fe93
Vladyslav <sharpilo.vladislav96@gmail.com>
2023-09-05 23:39:42 +0300

    Добавил админ панель

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import JSON, Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 
@@ -12,6 +13,7 @@ class Hotels(Base):
     rooms_quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
     
-
-
+    room = relationship("Rooms", back_populates='hotel')    
     
+    def __str__(self):
+        return f'Hotel - {self.name}, {self.location[:30]}'
--- app/main.py
+++ app/main.py
@@ -5,16 +5,20 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
 from redis import asyncio as aioredis
+from sqladmin import Admin
 
 from app.config import settings
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
+from app.database import engine
 
 from app.pages.router import router as router_pages
 from app.images.router import router as router_images
 
+from app.admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UsersAdmin
+from app.admin.auth import authentication_backend as auth_backend
 
 app = FastAPI()
 
@@ -47,3 +51,12 @@ app.add_middleware(
 async def startup():
     redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}", encoding='utf8', decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="cache")
+   
+    
+
+admin = Admin(app, engine, authentication_backend=auth_backend)
+
+admin.add_view(UsersAdmin)
+admin.add_view(BookingsAdmin)
+admin.add_view(HotelsAdmin)
+admin.add_view(RoomsAdmin)
